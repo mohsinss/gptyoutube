3cf381351f1256e90d94477fe616e590c974ffc3
Mohsin <92655551+mohsinss@users.noreply.github.com>
2023-03-08 23:29:46 -0800

    sec commit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from langchain.document_loaders import YoutubeLoader
 from langchain.llms import OpenAI
 from langchain.chains.summarize import load_summarize_chain
+import streamlit as st
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+st.set_page_config(page_title="Positive News", page_icon=":robot:")
+st.header("Positive News")
