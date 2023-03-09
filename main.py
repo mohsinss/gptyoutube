e254a21854e9b29d9c851ff7198e28531e396098
Mohsin <92655551+mohsinss@users.noreply.github.com>
2023-03-09 10:28:15 -0800

    youtube setup

--- main.py
+++ main.py
@@ -11,3 +11,21 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 st.set_page_config(page_title="Positive News", page_icon=":robot:")
 st.header("Positive News")
+
+def get_text():
+    input_text = st.text_area(label="News Input", label_visibility='collapsed', placeholder="Your News...", key="email_input")
+    return input_text
+
+email_input = get_text()
+
+if len(email_input.split(" ")) > 700:
+    st.write("Please enter a shorter Text. The maximum length is 700 words.")
+    st.stop()
+
+loader = YoutubeLoader.from_youtube_url("input_text", add_video_info=True)
+result = loader.load()
+
+print (type(result))
+print (f"Found video from {result[0].metadata['author']} that is {result[0].metadata['length']} seconds long")
+print ("")
+print (result)
